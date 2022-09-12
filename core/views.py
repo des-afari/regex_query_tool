5a1c6c10b622aa8eac78f7a63ee2057e500cb24d
Desmond Afari <afaridesmond@gmail.com>
2022-09-12 20:16:22 +0000

    new save

--- core/views.py
+++ core/views.py
@@ -3,17 +3,9 @@ from .forms import RegexForm
 
 # Create your views here.
 def index(request):
-    if request.method == 'POST' and 'regex_submit' in request.POST:
-        form = RegexForm()
+    form = RegexForm()
 
-        if form.is_valid():
-            pass
-
-    else:
-        form = RegexForm()
-        
-        context = {
-            'form': form
-        }
-
-        return render(request, 'index.html', context)
+    context = {
+        'form': form
+    }
+    return render(request, 'index.html', context)
--- regex_query_tool/urls.py
+++ regex_query_tool/urls.py
@@ -1,9 +1,9 @@
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
