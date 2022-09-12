d8b5d93c54ee17e558bd45b4c41b1759cda37156
Desmond Afari <afaridesmond@gmail.com>
2022-09-12 20:56:05 +0000

    added regex code

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
+import re
 # Create your tests here.
+sentence = "This is a sentence"
+
+
+pattern = re.compile(r'/w+')
+print(pattern)
+
+matches = pattern.finditer(sentence)
+
+# for match in matches:
--- core/views.py
+++ core/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import RegexForm
+import re
 
 # Create your views here.
 def index(request):
     if request.method == 'POST' and 'regex_submit' in request.POST:
-        print('This works')
-        return redirect('index')
-        
+        expression = request.POST['expression']
+        text = request.POST['text']
+
+        pattern = re.compile(expression)
+
+        matches = pattern.finditer(text)
+
+        for match in matches:
+            messages.info(request, match.group(0))
+            return redirect('index')        
 
     else:
         form = RegexForm()
